12d1a952fcc3cac00e5ec4398e2a27167ac1f929
Chuck Jungmann <chuckj60@gmail.com>
2019-11-07 12:51:12 -0600

    Added function to demonstrate new inisection object.

--- ritest.c
+++ ritest.c
@@ -1,3 +1,5 @@
+// -*- compile-command: "cc -Wall -Werror -o ritest ritest.c -lreadini" -*-
+
 #include <stdio.h>
 #include <readini.h>
 
@@ -27,6 +29,30 @@ void use_iniline(const struct iniline *lines)
    }
 }
 
+void use_inifile(const struct inisection *section)
+{
+   const struct inisection *s_ptr = section;
+   const struct iniline *i_ptr;
+   while (s_ptr)
+   {
+      printf("[34;1m%s[m\n", s_ptr->section_name);
+
+      i_ptr = s_ptr->lines;
+      while (i_ptr)
+      {
+         printf("  %s", i_ptr->tag);
+         if (i_ptr->value)
+            printf(" : %s", i_ptr->value);
+         printf("\n");
+         
+         i_ptr = i_ptr->next;
+      }
+
+      s_ptr = s_ptr->next;
+   }
+}
+
+
 void use_file_to_read(int fd)
 {
    read_section(fd, "bogus", use_iniline);
@@ -41,6 +67,12 @@ int main(int argc, char** argv)
 
    // Test service function
    cb_open("./demo.ini", use_file_to_read);
+
+   printf("Finished with first test.  Now on to test that reads the entire file.\n");
+
+   get_inifile("./demo.ini", use_inifile);
+
+   
    
    return 0;
 }
