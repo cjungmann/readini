fbfae41fbbaa052ff0d0f80bb9e35dd95a2fa937
Chuck Jungmann <chuckj60@gmail.com>
2019-11-08 20:59:29 -0600

    Moved functions around according to their category. Fixed memory bug in ri_parse_line_info(). Added generic void* to sparse read method to pass-along the state.

--- readini.h
+++ readini.h
@@ -22,17 +22,24 @@ typedef struct ri_section
 /**
  * Callback function pointer typedefs for *ri_open_section()* and *ri_open_file()*
  */
-typedef void (*ri_File_User)(int fh);
-typedef void (*ri_Lines_Browser)(const ri_Line *lines);
-typedef void (*ri_Sections_Browser)(const ri_Section *section);
+typedef void (*ri_File_User)(int fh, void* data);
+typedef void (*ri_Lines_Browser)(int fh, const ri_Line* lines, void* data);
+typedef void (*ri_Sections_Browser)(const ri_Section* section);
 
-void ri_open(const char *path, ri_File_User cb_file_user);
-void ri_open_section(int fh, const char *section_name, ri_Lines_Browser cb_lines_browser);
-void ri_open_file(const char *filepath, ri_Sections_Browser ifu);
-
+/**
+ * The three following functions offer extra control.  Open and
+ * read only what's needed.  Using these functions will conserve
+ * memory at the cost of more and more complicated code.
+ */
+void ri_open(const char *path, ri_File_User cb_file_user, void* data);
+void ri_open_section(int fh, const char *section_name, ri_Lines_Browser cb_lines_browser, void* data);
 const char* ri_find_value(const ri_Line* lines_head,
                           const char* tag_name);
+
+/** Simplest access: open file, fully-read it, then query the contents. **/
+void ri_read_file(const char *filepath, ri_Sections_Browser cb_sections_browser);
 
 const char* ri_find_section_value(const ri_Section* sections_head,
                                   const char* section_name,
                                   const char* tag_name);
+
